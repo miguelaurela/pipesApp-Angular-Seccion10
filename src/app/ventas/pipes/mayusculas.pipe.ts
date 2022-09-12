cf090bef819a1111a6a7cb4b091f1536986f4e29
Miguel Aurela <miguelaurela2804@gmail.com>
2022-09-12 16:59:50 -0500

    segundo commit

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe,PipeTransform } from "@angular/core";
+@Pipe({
+    name:'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform{
+     transform(value:string):string {
+         return 'HOLA MUNDO!';
+     }
+}
